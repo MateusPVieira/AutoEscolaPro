f4937e88347ae6958a2a9a1baf43ead29357a696
MuriloSdaSilva <murilor72003@outlook.com>
2023-06-04 18:49:45 -0300

    Implementação da tela cadastrar usuario e gerenciar usuarios(Não completadas)

---
+++ src/main/java/br/edu/ifsp/application/controller/InsertUserUIController.java
@@ -0,0 +1,11 @@
+package br.edu.ifsp.application.controller;
+
+import javafx.event.ActionEvent;
+
+public class InsertUserUIController {
+    public void cancelButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void submitButtonAction(ActionEvent actionEvent) {
+    }
+}
