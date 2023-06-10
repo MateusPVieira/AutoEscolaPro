f0ea3ad6a8de178b2df0bd0ca05d841327f3e781
StefhaniAlkin <stefhani.alkin@gmail.com>
2023-06-10 17:11:23 -0300

    Um pouco de interface

---
+++ src/main/java/br/edu/ifsp/application/controller/StudentManagementUIContoller.java
@@ -0,0 +1,17 @@
+package br.edu.ifsp.application.controller;
+
+import javafx.event.ActionEvent;
+
+public class StudentManagementUIContoller {
+    public void createStudent(ActionEvent actionEvent) {
+    }
+
+    public void deleteStudent(ActionEvent actionEvent) {
+    }
+
+    public void editStudent(ActionEvent actionEvent) {
+    }
+
+    public void detailStudent(ActionEvent actionEvent) {
+    }
+}
